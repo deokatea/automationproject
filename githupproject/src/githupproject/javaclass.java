60f2d632d1a19ca1a826bb8eeffb161723b58ea5
HP <HP@DEOKATE>
2021-10-28 15:29:11 +0530

    create new class

---
+++ githupproject/src/githupproject/javaclass.java
@@ -0,0 +1,17 @@
+package githupproject;
+
+public class javaclass {
+	
+	
+	
+	int a = 20;
+	int b =10;
+	int c=a+b;
+	public static void main(String[] args) {
+		
+		javaclass x = new javaclass();
+	System.out.println(x.c);
+		
+	}
+
+}
